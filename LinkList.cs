d7b8c92e7ceab541c1b5c492532cddfeebead853
Eli Fish <elifish607@gmail.com>
2024-09-04 14:04:04 +0300

    aaaa

--- LinkList.cs
+++ LinkList.cs
@@ -10,44 +10,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Day_1
 {
 
-    public class Node
-    {
-        private int Value;
-        private Node Next;
-
-        public Node(int value)
-        {
-            Value = value;
-            Next = null;
-        }
-
-        public int GetValue()
-        {
-            return Value;
-        }
-
-        public Node GetNext()
-        {
-            return Next;
-        }
-
-        public void SetValue(int value)
-        {
-            this.Value = value;
-        }
-
-        
-
-        public void SetNext(Node next)
-        {
-            this.Next = next;
-        }
-
-        
-
-
-
-    }
+    
 
 
 
